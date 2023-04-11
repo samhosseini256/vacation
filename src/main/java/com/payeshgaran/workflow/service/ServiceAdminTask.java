368ee15dc17233c2a557f5f096c6519781a22f10
Aziz Hosseini <shosseini@payeshgaran.net>
2023-04-11 14:27:43 +0330

    set random user to assign task

--- src/main/java/com/payeshgaran/workflow/service/ServiceAdminTask.java
+++ src/main/java/com/payeshgaran/workflow/service/ServiceAdminTask.java
@@ -7,6 +7,10 @@ import org.camunda.bpm.engine.delegate.TaskListener;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 @Service
 public class ServiceAdminTask implements TaskListener {
@@ -21,8 +25,21 @@ public class ServiceAdminTask implements TaskListener {
     @Override
     public void notify(DelegateTask delegateTask) {
 
-        taskService.setAssignee(delegateTask.getId(),"user1");
+//        taskService.setAssignee(delegateTask.getId(),"user1");
 
+        taskService.setAssignee(delegateTask.getId(),getRandomUsername());
+    }
+
+    private String getRandomUsername(){
+        List <String> randomUsername = new ArrayList<>();
+        randomUsername.add("user1");
+        randomUsername.add("user2");
+        randomUsername.add("user3");
+        randomUsername.add("user4");
+
+        Random random = new Random();
+
+        return randomUsername.get(random.nextInt(randomUsername.size()));
 
     }
 
