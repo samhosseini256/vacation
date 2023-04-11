e3b8267166baa329c65809a6e0ff6967dcf49e0a
Aziz Hosseini <shosseini@payeshgaran.net>
2023-04-11 13:18:02 +0330

    Initial commit

---
+++ src/main/java/com/payeshgaran/workflow/Application.java
@@ -0,0 +1,18 @@
+package com.payeshgaran.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Map;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String... args) {
+    SpringApplication.run(Application.class, args);
+
+
+
+  }
+
+}
---
+++ src/main/java/com/payeshgaran/workflow/service/ServiceAdminTask.java
@@ -0,0 +1,30 @@
+package com.payeshgaran.workflow.service;
+
+import org.camunda.bpm.engine.IdentityService;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.delegate.TaskListener;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+
+@Service
+public class ServiceAdminTask implements TaskListener {
+
+    @Inject
+    private TaskService taskService;
+
+    @Inject
+    private IdentityService identityService;
+
+
+    @Override
+    public void notify(DelegateTask delegateTask) {
+
+        taskService.setAssignee(delegateTask.getId(),"user1");
+
+
+    }
+
+
+}
